adc1d88939655d2144a37de9bf95849d08031134
Volker Krause <vkrause@kde.org>
2016-04-18 21:32:23 +0200

    Implement default color handling.

--- src/format.cpp
+++ src/format.cpp
@@ -17,9 +17,50 @@
 
 #include "format.h"
 
+#include <QDebug>
 #include <QXmlStreamReader>
 
 using namespace KateSyntax;
+
+// TODO move this outside and make it changeable
+struct def_format_t {
+    quint32 normalColor;
+};
+
+static const def_format_t default_formats[] {
+    { 0x00000000 }, // Normal
+    { 0xff1f1c1b }, // Keyword
+    { 0xff644a9b }, // Function
+    { 0xff0057ae }, // Variable
+    { 0xff1f1c1b }, // ControlFlow
+    { 0xff1f1c1b }, // Operator
+    { 0xff644a9b }, // BuiltIn
+    { 0xff0095ff }, // Extension
+    { 0xff006e28 }, // Preprocessor
+    { 0xff0057ae }, // Attribute
+    { 0xff924c9d }, // Char
+    { 0xff3daee9 }, // SpecialChar
+    { 0xffbf0303 }, // String
+    { 0xff0057ae }, // VerbatimString
+    { 0xffff5500 }, // SpecialString
+    { 0xffff5500 }, // Import
+    { 0xff0057ae }, // DataType
+    { 0xffb08000 }, // DecVal
+    { 0xffb08000 }, // BaseN
+    { 0xffb08000 }, // Float
+    { 0xffaa5500 }, // Constant
+    { 0xff898887 }, // Comment
+    { 0xff607880 }, // Documentation
+    { 0xffca60ca }, // Annotation
+    { 0xff0095ff }, // CommentVar
+    { 0xff0057ae }, // RegionMarker
+    { 0xffb08000 }, // Information
+    { 0xffbf0303 }, // Warning
+    { 0xffbf0303 }, // Alert
+    { 0xffbf0303 }, // Error
+    { 0xff006e28 }, // Others
+
+};
 
 static Format::DefaultFormat stringToDefaultFormat(const QStringRef &str)
 {
@@ -82,17 +123,19 @@ QString Format::name() const
 
 bool Format::isNormal() const
 {
-    return !m_hasColor && !m_hasSelColor && !m_hasBgColor && !m_bold && !m_italic && !m_underline && !m_strikeout;
+    return !hasColor() && !m_hasSelColor && !m_hasBgColor && !m_bold && !m_italic && !m_underline && !m_strikeout;
 }
 
 bool Format::hasColor() const
 {
-    return m_hasColor;
+    return m_hasColor || (default_formats[m_default].normalColor & 0xff000000);
 }
 
 QColor Format::color() const
 {
-    return m_color;
+    if (m_hasColor)
+        return m_color;
+    return QColor(QRgb(default_formats[m_default].normalColor));
 }
 
 bool Format::hasBackgroundColor() const

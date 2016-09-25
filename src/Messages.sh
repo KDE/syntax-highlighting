c304561b273c04d708cdd2da6511a70b8ddaa902
Volker Krause <vkrause@kde.org>
2016-09-25 12:09:22 +0200

    Extract theme names for translation

--- src/Messages.sh
+++ src/Messages.sh
@@ -5,4 +5,11 @@
 $EXTRACTATTR --attr=language,name,Language --attr="language,section,Language Section" ../data/*.xml >> rc.cpp || exit 12
 sed -i -e 's/^i18nc/QT_TRANSLATE_NOOP/' rc.cpp
 
+grep --no-filename '"name"' ../data/themes/*.theme > rc_themes.cpp
+sed -i -e 's/"name"/QT_TRANSLATE_NOOP("Theme", /' rc_themes.cpp
+sed -i -e 's/://' rc_themes.cpp
+sed -i -e 's/,$/)/' rc_themes.cpp
+
 $EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/syntaxhighlighting5_qt.pot
+
+rm rc_themes.cpp

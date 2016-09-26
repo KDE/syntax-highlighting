03ae61fe296fb517b2c81dcaa789e229b28f8522
Burkhard Lück <lueck@hube-lueck.de>
2016-09-26 09:49:57 +0200

    fix Messages.sh

--- src/Messages.sh
+++ src/Messages.sh
@@ -2,7 +2,7 @@
 
 # extract language and section names from XML syntax definition files
 # and adapt for lupdate-based extraction to match the rest of the code
-$EXTRACTATTR --attr=language,name,Language --attr="language,section,Language Section" ../data/*.xml >> rc.cpp || exit 12
+$EXTRACTATTR --attr=language,name,Language --attr="language,section,Language Section" ../data/syntax/*.xml >> rc.cpp || exit 12
 sed -i -e 's/^i18nc/QT_TRANSLATE_NOOP/' rc.cpp
 
 grep --no-filename '"name"' ../data/themes/*.theme > rc_themes.cpp

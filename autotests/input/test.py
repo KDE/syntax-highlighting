92b27ca747693aed4398acb684d5956d73f372e6
Volker Krause <vkrause@kde.org>
2016-10-03 09:54:23 +0200

    Add unit test for Python
    Will be useful to evaluate indention-based folding support.

---
+++ autotests/input/test.py
@@ -0,0 +1,28 @@
+# comment with ALERT
+
+''' multiline
+    comment
+    ###
+    '''
+
+def func(x):
+    """ API docs """
+    if x == 42:
+        func()
+        c1 = {}
+        c2 = {
+            2.4,
+            0x42
+            }
+        a1 = []
+        a2 = [
+            "a", 032, (
+                )]
+    else:
+        print("""multi
+            line
+            string""")
+        print("single \
+                continued line ")
+        print('single line')
+    return float(len(a2))

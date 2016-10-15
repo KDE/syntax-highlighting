10430a59f5c59857fd54beba346ac50130348d16
Dominik Haumann <dhaumann@kde.org>
2016-10-15 21:43:38 +0200

    More API documentation for FoldingRegion

--- src/lib/foldingregion.h
+++ src/lib/foldingregion.h
@@ -28,25 +28,57 @@ namespace SyntaxHighlighting {
 class KF5SYNTAXHIGHLIGHTING_EXPORT FoldingRegion
 {
 public:
+    /**
+     * Defines whether a FoldingRegion starts or ends a folding region.
+     */
     enum Type {
+        //! Used internally as indicator for invalid FoldingRegion%s.
         None,
+        //! Indicates the start of a FoldingRegion.
         Begin,
+        //! Indicates the end of a FoldingRegion.
         End
     };
 
-    /** Constructs an invalid folding region.
-     *  To obtain valid instances, see AbstractHighlighter::applyFolding().
+    /**
+     * Constructs an invalid folding region, meaning that isValid() returns @e false.
+     * To obtain valid instances, see AbstractHighlighter::applyFolding().
      */
     FoldingRegion();
 
-    /** Returns @c true if this is a valid folding region.
-     *  That is, if the type is not None.
+    /**
+     * Returns @c true if this is a valid folding region.
+     * A valid FoldingRegion is defined by a type() other than Type::None.
+     *
+     * @note The FoldingRegion%s passed in AbstractHighlighter::applyFolding()
+     *       are always valid.
      */
     bool isValid() const;
 
-    /** Returns a unique identifier for this folding region. */
+    /**
+     * Returns a unique identifier for this folding region.
+     *
+     * As example, the C/C++ highlighter starts and ends a folding region for
+     * scopes, e.g.:
+     * \code
+     * void foo() {     // '{' starts a folding region
+     *     if (bar()) { // '{' starts a (nested) folding region
+     *     }            // '}' ends the (nested) folding region
+     * }                // '}' ends the outer folding region
+     * \endcode
+     * In this example, all braces '{' and '}' have the same id(), meaning that
+     * if you want to find the matching closing region for the first opening
+     * brace, you need to do kind of a reference counting to find the correct
+     * closing brace.
+     */
     quint16 id() const;
-    /** Returns whether this is the begin or end of a region. */
+
+    /**
+     * Returns whether this is the begin or end of a region.
+     *
+     * @note The FoldingRegion%s passed in AbstractHighlighter::applyFolding()
+     *       are always valid, i.e. either Type::Begin or Type::End.
+     */
     Type type() const;
 
 private:

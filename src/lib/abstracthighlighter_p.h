bc7ac5431ddd2dd96f0b7b25a3e7c94b40a6bbe1
Volker Krause <vkrause@kde.org>
2016-11-06 14:03:05 +0100

    Fix typo in include guard
    Found by krazy.

--- src/lib/abstracthighlighter_p.h
+++ src/lib/abstracthighlighter_p.h
@@ -15,8 +15,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef KSYNTAXHIGHLIGHTING_ABSTRACTHIGHLIGHTERM_P_H
-#define KSYNTAXHIGHLIGHTING_ABSTRACTHIGHLIGHTERM_P_H
+#ifndef KSYNTAXHIGHLIGHTING_ABSTRACTHIGHLIGHTER_P_H
+#define KSYNTAXHIGHLIGHTING_ABSTRACTHIGHLIGHTER_P_H
 
 #include "definition.h"
 #include "theme.h"

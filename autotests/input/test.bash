12c6df3b6d958ac87a2a678b24b8e84c624e01e7
Dominik Haumann <dhaumann@kde.org>
2017-07-23 15:49:45 +0200

    Bash: Fix incorrect highlighting for command options
    FIXED-IN: 5.37
    BUG: 375245

--- autotests/input/test.bash
+++ autotests/input/test.bash
@@ -4,3 +4,9 @@ for i in `ls tests/auto/output/*.html`; do
     refFile=`echo $i | sed -e s,build,src, | sed -e s,output,reference, | sed -e s,.html,.ref.html,`
     cp -v $i $refFile
 done
+
+## >Settings >Configure Kate >Fonts & Colors >Highlitghing Text Styles >Scripts/Bash >Option >Change colors to some distinct color
+## 1- In following line the -ucode should not be colored as option
+
+pacman -Syu --needed intel-ucode grub
+pacman -syu --needed intel-ucode grub

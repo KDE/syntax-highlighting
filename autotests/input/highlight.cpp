a2343cae2bb50f9a64ffb0e18f45fdf702444fe4
Jonathan Poelen <jonathan.poelen@gmail.com>
2018-07-14 19:08:43 +0200

    C++: update for c++20 and fix some syntax errors
    Differential Revision: https://phabricator.kde.org/D13394

--- autotests/input/highlight.cpp
+++ autotests/input/highlight.cpp
@@ -3,9 +3,19 @@
 #include <cassert>
 #include <assert.h>
 #include "assert.h"
-#include "assert.hpp"
+#include "assert.hpp" // abc
 #include "path/assert.hpp"
+#include "assert.h"a
+#include "assert.h" a
+#include <cassert>a
+#include <cassert> a
+#include FOO() error
+#include_next <cassert> a
+#include_next <cassert> /* a
+ */ b
+#include PATH_IN_MACRO
 #include PATH_IN_MACRO()
+#include PATH_IN_MACRO(a, b)
 
 #define SOME_VAR 1
 #ifdef SOME_VAR
@@ -19,6 +29,16 @@ three
     char const s[] = "a\\b"                          \
     "c\nd"
 
+# define VARIADIC(a, ...)          \
+    f(a##a)                        \
+    f(__VA_ARGS__)                 \
+    f(#__VA_ARGS__)                \
+    f(__VA_ARGS__)                 \
+    f(0 __VA_OPT__(,) __VA_ARGS__) \
+    x __VA_OPT__(= { __VA_ARGS__ })
+
+# define MACRO() BAD \ ESCAPED
+
 # error dds
 # warning dds
 # line 2 "file.cpp"
@@ -26,6 +46,24 @@ three
 // OK(L, a) -> L"a"
 # define OK(x, y) x###y
 # define BAD(x, y) x####y
+# define A /* multi line
+with comment */ expr
+# define A /* multi line
+with comment */
+23
+#else // x
+#42 // gcc extension = #line 42
+
+// error
+#wrong
+# wrong
+#endif x
+#if DS()
+#else x
+#else /* */x
+#else /* x
+y */ z
+#endif
 
 static int g_global;
 
@@ -39,6 +77,8 @@ struct class1
     {}
         catch(...)
     {}
+
+    class1(class1&&) = default;
 
     ~class1()
     {}
@@ -72,6 +112,9 @@ public:
     value_type m_member3;
 
     static int s_static;
+    static constexpr int s_static;
+    static inline int s_static;
+    static inline constexpr int s_static;
 };
 
 constexpr struct : xyz
@@ -102,24 +145,132 @@ uint64_t namespaces()
 
 #if 1
     double foo(const A);
-#else
+#else // else
     double foo(const A);
-#endif
+#endif // end
 
 #if 0
     double foo(const A);
-#else
+#else // else
     double foo(const A);
-#endif
+#endif // end
+
+#if 1
+    double foo(const A);
+#elif 1
+    double foo(const A);
+#elif 0
+    double foo(const A);
+#endif // end
+
+#if 0
+    double foo(const A);
+#elif 1
+    double foo(const A);
+#elif 0
+    double foo(const A);
+#endif // end
+
+#if 0
+    double foo(const A);
+#elif a
+    double foo(const A);
+#elif 0
+    double foo(const A);
+#elif a
+    double foo(const A);
+#else // else
+    double foo(const A);
+#endif // end
+
+#if 0 // blah blah
+    double foo(const A);
+#elif 1 // blah blah
+    double foo(const A);
+#else // else
+    double foo(const A);
+#endif // end
+
+#if 0 || a
+    double foo(const A);
+#else // else
+    double foo(const A);
+#endif // end
+
+#if 1 || a
+    double foo(const A);
+#else // else
+    double foo(const A);
+#endif // end
+
+#if 0 && a
+    double foo(const A);
+#else // else
+    double foo(const A);
+#endif // end
+
+#if 1 && a
+    double foo(const A);
+#else // else
+    double foo(const A);
+#endif // end
+
+#if a
+    double foo(const A);
+#elif 0
+    double foo(const A);
+#endif // end
+
+#if a
+    double foo(const A);
+#elif 1
+    double foo(const A);
+#endif // end
+
+#if a
+    double foo(const A);
+#elif a
+    double foo(const A);
+#endif // end
 
 int bar(void*p, void * pp)
 {
 # if 0
     double foo();
-# else
+# else // else
     double foo();
-# endif
+# endif // end
 }
+
+#if abc 0
+    double foo();
+#endif // end
+
+#if xxx
+    double foo();
+#else
+    double foo();
+#endif // end
+
+#if xxx
+    double foo();
+#elif xxx // elseif
+    double foo();
+#elif xxx // elseif
+    double foo();
+#endif // end
+
+// error
+#
+#d
+# d
+#if
+#elif
+#endif
+#ifndef
+#endif
+#ifdef 0
+#endif // end
 
 static uint64_t intWithSuffix = 42ull;
 static long intWithSuffixAndPrefix = 0b0101L;
@@ -130,7 +281,7 @@ static uint64_t invalidHexNum = 0xGLDFKG;
 static char binNum = 0b0101010;
 
 static int64_t intWithSuffix = -42LL;
-static long intWithSuffixAndPrefix = -0b0101L; // BUG
+static long intWithSuffixAndPrefix = -0b0101L;
 static int octNum = -07232;
 static int invalidOctNum = -09231;
 static int64_t hexNum = -0xDEADBEEF42;
@@ -145,6 +296,9 @@ static uint64_t hexNumWithSep = 0xD'EAD'BE'EF'42;
 static uint64_t invalidHexNumWithSep = 0xGLD'FKG;
 static char binNumWithSep = 0b0'1010'10;
 
+static uint64_t invalidSep = 42'ull;
+static uint64_t invalidSep = 42';
+
 static double d1 = 42.;
 static double d2 = .42;
 static double d2a = -0.49;
@@ -154,30 +308,66 @@ static double d4 = .2e-12;
 static double d5 = 32.e+12;
 static double invalidD1 = 32.e+a12;
 static float floatQualifier = 23.123f;
-// TODO c++17
+// Hexadecimal floating point (c++17)
 static double d6 = 0x1ffp10;
 static double d7 = 0X0p-1;
 static double d8 = 0x1.p0;
-static double d9 = 0xf.p-1;
+static double d9 = 0xf.p-1L;
 static double d10 = 0x0.123p-1;
 static double d11 = 0xa.bp10l;
 static double invalidD2 = 0x0.123p-a;
 static float floatQualifier = 0xf.p-1f;
 
+60min; // c++17
+23._f
+23._fd
+2.3_f
+2.3_fd
+2._f
+2._fd
+2e4_f
+2e4_fd
+
+// error
+23.fd
+2e_fd
+2e
+1.y
+1.0_E+2.0
+1.0_E +2.0 // ok
+1_p+2
+1_p +2 // ok
+4s.count()
+4s. count()
+4s .count() // ok
+
 static bool yes = true;
 static bool no = false;
 
-
-static const char c1 = 'c';
-static const char       c1a = u8'c'; // TODO c++17
-static const char16_t   c1b = u'c';
-static const char32_t   c1c = U'c';
-static const wchar_t    c1d = L'c';
+// *char*
+static const char     c1 = 'c';
+static const char     c1a = u8'c'; // utf-8 char (c++17)
+static const char16_t c1b = u'c';
+static const char32_t c1c = U'c';
+static const wchar_t  c1d = L'c';
 static const char c2 = '\n';
 static const char c2a = '\120'; // octal
 static const char c2b = '\x1f'; // hex
 static const char c2c = '\'';
 static const char c2d = '\\';
+static const wchar_t c2e = L'\x1ff'; // hex
+static const wchar_t c2e = U'\x1fffffff'; // hex
+// error
+'\x123';
+'\u1234';
+'\U12345678';
+U'\u12345';
+u'\u123';
+U'\U1234567';
+U'\U123456789';
+U'\x123456789';
+
+// string
 static const char* c3 = "string";
 static const char* c4 = "\"string\n\t\012\x12\"";
 static const char* c5 = "multiline \
@@ -195,18 +385,24 @@ static const wchar_t*   rawString1d = LR"(string\nstring)";
 static const char* rawString2 = R"ab(string\nstring)ab";
 static const char* rawString3 = R"ab(string
 string)ab";
+" %d %df fd" U"ds %d" R"(a%d)";
+  "\x123xsk";
+ u"\x123xsk";
+// error
+u8"\x123xsk";
 
 // UDL (c++11)
 
-operator""_a(const char*); // BUG operator not colorized
-operator ""_a(const char*); // BUG colorized or
-operator "" _a(const char*); // BUG or not colorized
+operator""_a(const char*);
+operator ""_a(const char*);
+operator "" _a(const char*);
+// invalid suffix
 operator "" a(const char*);
-operator ""a(const char*); // BUG
-operator""a(const char*); // BUG
+operator ""a(const char*);
+operator""a(const char*);
 
-"string"_s; // BUG colorized or
-"string"s; // BUG or not colorized
+"string"_s; // user
+"string"s; // standard
 "string"_s-b; // -b is not part of the string
 
 // Macro
@@ -227,6 +423,10 @@ void foo([[carries_dependency]] int);
 [[opt(1), debug]]
 [[using CC: opt(1), debug]] // c++17
 [[using CC: CC::opt(1)]] // c++17
+
+[[gnu::assume_aligned(3'2l,2)]] void* f();
+[[using gnu: assume_aligned(3)]]
+[[clang::assume_aligned(3)]]
 
 void f([[maybe_unused]] int val)
 {
@@ -268,8 +468,6 @@ goto label;
 //BEGIN region
 // TODO comment FIXME comment ### comment BUG comment
 //END region
-
-#wrong
 
 // \brief blah blah
 /// \brief blah blah

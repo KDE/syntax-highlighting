48c8c7acd4768342ba79c5b1feeda06f0f803281
Nibaldo González <nibgonz@gmail.com>
2018-08-09 04:56:30 -0400

    Java: add binaries & hex-float, and support underscores in numbers
    Summary:
    Some improvements in the highlighting of literal numbers:
    
    * Add binaries: `0b1010` [1][3].
    
    BUG: 386391
    
    * Allow underscores in numbers (Float, Int, Hex, Octal & Binary) [2][3][4].
    
    NOTE: The exact format of the numbers supported can be found in ref. [3] & [4].
    
    * Add hexadecimal floating point: `0xA21.F18p+123` [4].
    
    * Add missing suffixes in numbers: "L" in Hex & Octal, and "D" in Float [3][4].
    
    **References:**
    * [1] Java (8) Documentation. Binary Literals: https://docs.oracle.com/javase/8/docs/technotes/guides/language/binary-literals.html
    * [2] Java (8) Documentation. Underscores in Numeric Literals: https://docs.oracle.com/javase/8/docs/technotes/guides/language/underscores-literals.html
    * [3] Java Language Specification. Chapter 3, Lexical Structure. [3.10.1] Integer Literals: https://docs.oracle.com/javase/specs/jls/se10/html/jls-3.html#jls-3.10.1
    * [4] Java Language Specification. Chapter 3, Lexical Structure. [3.10.2] Floating-Point Literals: https://docs.oracle.com/javase/specs/jls/se10/html/jls-3.html#jls-3.10.2
    
    Reviewers: dhaumann, cullmann, vkrause, #kate
    
    Reviewed By: cullmann, #kate
    
    Subscribers: ngraham, kwrite-devel, kde-frameworks-devel
    
    Tags: #kate, #frameworks
    
    Differential Revision: https://phabricator.kde.org/D14702

--- autotests/input/highlight.java
+++ autotests/input/highlight.java
@@ -35,5 +35,14 @@ public class Activity extends QtActivity
         int num = 1;
         num = 0x2;
         double f = .4e+49;
+        
+        long int_dec = 1__23_48l;
+        long int_oct = 072034__4l;
+        long int_hex = 0xa__12Ff_13L;
+        int int_bin = 0B1011___01;
+        
+        double float_hex = 0x1f__a2.d_f10P+1074D;
+        float float_dec = 1__24.1_32e-1____23F;
+        float[] float_d = {1.,1.E+3,1.E+3f,1.F,1.1E+3d,1.1E+3,1.1f,1.1,.1E+3f,.1E+3,.1f,.1,1E+3f,1E+3,1f,1d};
     }
 }

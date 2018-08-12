3418551190f66d50f040d42b31bec3b3f21fe2ff
Nibaldo González <nibgonz@gmail.com>
2018-08-12 07:28:17 -0300

    Add highlighting of JavaScript React
    Summary:
    Add syntax hightlighting of JSX files (ReactJS). Basically, this is JavaScript plus tags like XML.
    
    This uses the existing JavaScript highlighting file.
    This also applies to TSX files (TypeScript React).
    
    Reviewers: #framework_syntax_highlighting, dhaumann, cullmann
    
    Reviewed By: #framework_syntax_highlighting, cullmann
    
    Subscribers: kwrite-devel, cullmann, #framework_syntax_highlighting
    
    Tags: #frameworks, #framework_syntax_highlighting, #kate
    
    Differential Revision: https://phabricator.kde.org/D13692

---
+++ autotests/input/test.jsx
@@ -0,0 +1,50 @@
+/* email: <willy@wmail.com>
+ * URL: https://reactjs.org/*/
+
+import React from 'react';
+import { PhotoStory, VideoStory } from './stories';
+
+function Story(props) {
+  const SpecificStory = components[props.storyType];
+  return <SpecificStory story={ props.story } attr2="&ref;" attr3="Hello\n" />;
+} 
+
+function 
+<Tag attr1={ <tag/> function <noTag/> return class var 0x123 { } &noRef; hello() React.Component() } attr2="&ref;">
+	/* no comment*/ function <tag/> return class var 0x123 &ref; hello() React.Component()
+	.<tag/> anyWord <tag/>
+	{ function <tag> return class var 0x123 hello() React.Component() } 
+</Tag>
+
+<tag1> </tag1>
+<tag1> </Tag$>
+<Tag$> </tag>
+
+<tag/*comment*/attr1/*comment*/= /*comment*/"value"/*comment*/attr2 /*comment*/attr3='a' key/*comment*/key2 />
+
+// Detect Valid tags
+
+/* comment */ <tag></tag>
+{ /* comment 
+	*/ <Tag /> 
+	word <noTag/> . <noTag/> } <noTag/>
+return /* comment
+	multiline */ <tag/> /* comment */ <Tag/>
+&& /*comment*/ <Tag/>
+& /*comment*/ <noTag/>
+
+  <tag/>
+{ <hello>Hello</hello> }
+?<Tag />;
+[ <tag />  ( <tag />
+,<Tag/>    =<Tag/>
+&&<tag/>   ||<tag/>
+return <tag/> ;
+default<tag/> ;
+<Tag> <tag> <tag$/> </tag> return </Tag>
+
+anyWord<noTag>
+anyWord/*comment*/ <noTag/>
+.<noTag>
+&<notag> | <noTag/>
+% /* comment*/ <noTag/>

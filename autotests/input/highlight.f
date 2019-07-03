c579929d8667114ca1d440563d409f7c92cf00e6
Nibaldo González <nibgonz@gmail.com>
2019-07-03 20:19:03 +0200

    Fortran: implement free & fixed formats
    Summary:
    FEATURE: 372950
    
    Task T9775
    
    * Implement patch provided at: https://github.com/janusw/syntax-highlighting/commits/Fortran_2003_2008
    * The `fortran-fixed.xml` file corresponds to the original file (`fortran.xml`), which is renamed. The `fortran-free.xml` file uses `IncludeRules` and include-keywords to avoid replicated code.
    * Some unnecessary `RegExpr` rules are replaced for optimization.
    
    TODO: ask for MIT license.
    
    Reviewers: #framework_syntax_highlighting, dhaumann, cullmann
    
    Reviewed By: #framework_syntax_highlighting, cullmann
    
    Subscribers: nononux, kwrite-devel, kde-frameworks-devel
    
    Tags: #kate, #frameworks
    
    Differential Revision: https://phabricator.kde.org/D21713

---
+++ autotests/input/highlight.f
@@ -0,0 +1,28 @@
+* This file is an example to test the syntax highlighting file fortran-fixed.xml
+* (for fortran, fixed format)
+
+c <-- this is a comment in the old fortran 77 style (fixed form)
+c In the free form file, so we shouldn't use this kind of comments!
+c But fortran 90 still understands fixed form, when parsing sources with
+c the *.f extension.
+
+* this 'c' shouldn't be highlighted as a comment!
+ c
+
+*     Prints the values of e ** (j * i * pi / 4) for i = 0, 1, 2, ..., 7
+*         where j is the imaginary number sqrt(-1)
+
+PROGRAM CMPLXD
+    IMPLICIT COMPLEX(X)
+    PARAMETER (PI = 3.141592653589793, XJ = (0, 1))
+    DO 1, I = 0, 7
+        X = EXP(XJ * I * PI / 4)
+        IF (AIMAG(X).LT.0) THEN
+            PRINT 2, 'e**(j*', I, '*pi/4) = ', REAL(X), ' - j',-AIMAG(X)
+        ELSE
+            PRINT 2, 'e**(j*', I, '*pi/4) = ', REAL(X), ' + j', AIMAG(X)
+        END IF
+        FORMAT (A, I1, A, F10.7, A, F9.7)
+        CONTINUE
+    STOP
+END

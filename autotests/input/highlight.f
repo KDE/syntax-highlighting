fdd4d8defb116108e66be3b375ef12973ae4d02f
Arnaud Ruiz <arnaud.ruizsin@gmail.com>
2019-07-06 02:43:49 -0400

    Improve the Fortran fixed format syntax highlighting
    Summary:
    3 improvements:
    - Restore the comment highlighting when using the "!" character (comment all the line after the !, works whatever the position of the !)
    - The characters after the 72th column are highlighted as comments (they are ignored by the compiler)
    - The continuation character is highlighted (5 firsts characters are blank and the 6th is not blank and not a number, usually "&" or "*")
    
    Test Plan: Check the updated autotests/input/highlight.f
    
    Reviewers: nibags
    
    Reviewed By: nibags
    
    Subscribers: nibags, kwrite-devel, kde-frameworks-devel
    
    Tags: #kate, #frameworks
    
    Differential Revision: https://phabricator.kde.org/D22253

--- autotests/input/highlight.f
+++ autotests/input/highlight.f
@@ -12,17 +12,20 @@ c the *.f extension.
 *     Prints the values of e ** (j * i * pi / 4) for i = 0, 1, 2, ..., 7
 *         where j is the imaginary number sqrt(-1)
 
-PROGRAM CMPLXD
-    IMPLICIT COMPLEX(X)
-    PARAMETER (PI = 3.141592653589793, XJ = (0, 1))
-    DO 1, I = 0, 7
-        X = EXP(XJ * I * PI / 4)
-        IF (AIMAG(X).LT.0) THEN
-            PRINT 2, 'e**(j*', I, '*pi/4) = ', REAL(X), ' - j',-AIMAG(X)
-        ELSE
-            PRINT 2, 'e**(j*', I, '*pi/4) = ', REAL(X), ' + j', AIMAG(X)
-        END IF
-        FORMAT (A, I1, A, F10.7, A, F9.7)
-        CONTINUE
-    STOP
-END
+      PROGRAM CMPLXD
+          IMPLICIT COMPLEX(X)
+          PARAMETER (PI = 3.141592653589793, XJ = (0, 1))               this text should be highlighted as a comment because it is after the 72th column (ignored by the compiler)
+          DO 1, I = 0, 7
+              X = EXP(XJ * I * PI / 4)
+              IF (AIMAG(X).LT.0) THEN
+                  PRINT 2, 'e**(j*', I, '*pi/4) = ', ! I am an in-line comment
+     *                     REAL(X), ' - j',-AIMAG(X) ! the character at the 6th column indicate a continuation line and should be highlighted as a continuation character
+              ELSE
+                  PRINT 2, 'e**(j*', I, '*pi/4) = ',
+     *                     REAL(X), ' + j', AIMAG(X)
+              END IF
+              FORMAT (A, I1, A, F10.7, A, F9.7)
+              CONTINUE
+          ENDDO
+          STOP
+      END

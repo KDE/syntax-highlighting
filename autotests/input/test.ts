1766185fe580a05b0ebcb6b7110026d498623735
Nibaldo González <nibgonz@gmail.com>
2019-07-19 15:36:07 -0400

    TypeScript: fix keywords in conditional expressions
    Summary:
    Now, keywords are highlighted before ":" in conditional expressions, for example:
    ```
    a = b ? false : true;
    ```
    
    Reviewers: #framework_syntax_highlighting, dhaumann, cullmann
    
    Reviewed By: #framework_syntax_highlighting, cullmann
    
    Subscribers: kwrite-devel, kde-frameworks-devel
    
    Tags: #kate, #frameworks
    
    Differential Revision: https://phabricator.kde.org/D22567

--- autotests/input/test.ts
+++ autotests/input/test.ts
@@ -179,3 +179,11 @@ dt = ((dt[0] * 1e9 + dt[1]) / 1e6).toFixed(3).replace(/\.?0+$/, '') + 'ms';
 function isNumber(x: any): x is number {
     return typeof x === "number";
 }
+
+// Conditional expressions (highlight keywords before ":")
+class C {
+    w = () =>
+        this.a() ? true : this.b() ? false : true;
+    z = () =>
+        this.b() ? hello : k; 
+}

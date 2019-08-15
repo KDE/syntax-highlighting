f42b6130397e3509adcafd53ebb6706a24f1bd65
Jonathan Poelen <jonathan.poelen@gmail.com>
2019-08-15 20:35:03 +0200

    C++: contracts are not in C++20
    Summary: For C ++ 23 or C ++ 26 with perhaps a syntax change
    
    Reviewers: #framework_syntax_highlighting, dhaumann, cullmann
    
    Reviewed By: #framework_syntax_highlighting, cullmann
    
    Subscribers: kwrite-devel, kde-frameworks-devel
    
    Tags: #kate, #frameworks
    
    Differential Revision: https://phabricator.kde.org/D23137

--- autotests/input/highlight.cpp
+++ autotests/input/highlight.cpp
@@ -428,8 +428,6 @@ __cplusplus;
 
 // Attributes
 
-int f(int i) [[expects: i > 0]] [[ensures audit x: x < 1]];
-
 [[noreturn]] void foo();
 [[deprecated]] void foo();
 [[deprecated("because")]] void foo();

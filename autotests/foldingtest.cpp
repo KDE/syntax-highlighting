342dc538e01060c8a48465cf31fcfc7c13bab863
Christoph Cullmann <cullmann@kde.org>
2020-08-22 18:40:22 +0200

    add autotest for: .kdesrc-buildrc, fix handling of files with leading .

--- autotests/foldingtest.cpp
+++ autotests/foldingtest.cpp
@@ -127,7 +127,7 @@ private Q_SLOTS:
         QTest::addColumn<QString>("syntax");
 
         const QDir dir(QStringLiteral(TESTSRCDIR "/input"));
-        for (const auto &fileName : dir.entryList(QDir::Files | QDir::NoSymLinks | QDir::Readable, QDir::Name)) {
+        for (const auto &fileName : dir.entryList(QDir::Files | QDir::NoSymLinks | QDir::Readable | QDir::Hidden, QDir::Name)) {
             const auto inFile = dir.absoluteFilePath(fileName);
             if (inFile.endsWith(QLatin1String(".syntax")))
                 continue;

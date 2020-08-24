d8e7df392cd8b1a566768a1b892b86a6edf381e5
Christoph Cullmann <cullmann@kde.org>
2020-08-24 22:25:27 +0200

    avoid test duplication

--- autotests/input/highlight.cpp
+++ autotests/input/highlight.cpp
@@ -524,3 +524,28 @@ int main()
     [](){%>();
 }
 <:<:fallthrough]]
+
+/**
+ * multi-line comment
+ */
+
+/* comment */
+{ { } {
+//BEGIN
+}
+//END
+}
+
+#if 0
+#elif 1
+#else
+#endif
+
+#if 1
+int x; // variable shall not be grey
+#endif
+#if defined (A)
+int y; // variable shall not be grey
+#elif defined (B)
+int z; // variable shall not be grey
+#endif

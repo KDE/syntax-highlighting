dd9ffd2119e3261c10e81354050a4ff874199524
Christoph Cullmann <cullmann@kde.org>
2020-08-24 21:26:47 +0200

    collapse tests for same language

--- autotests/input/highlight.glsl
+++ autotests/input/highlight.glsl
@@ -60,3 +60,30 @@ radians degrees sin cos tan asin acos atan
 
 // some of the std variables
 gl_Position gl_PointSize gl_ClipVertex
+
+#version 330 core
+
+// single line comment
+
+/* single line commonet */
+
+/*
+ multi line comment
+ */
+
+in ColorFormat {
+    vec3 color;
+} fs_in;
+
+out vec4 fragColor;
+
+vec3 fun(const in vec3 foo) {
+    foo[2] = foo.x;
+
+    return foo;
+}
+
+void main()
+{
+    fragColor = vec4( fs_in.color, 1.0 );
+}

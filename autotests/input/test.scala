0d2af27875ef2df9b0b174a8acffd4eb1ad928ea
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-12-07 02:33:30 +0100

    Scala: fix some issues and other improvements
    - add string interpolation
    - fix escaped char
    - fix integer format
    - other improvements

---
+++ autotests/input/test.scala
@@ -0,0 +1,11 @@
+object HelloWorld {
+	def main(args: Array[String]): Unit = {
+		println("""a""a\n\uuu2132\u23"""""""")
+		if (true == null) println(raw"""
+\n$a${1 + 2}
+""")
+		obj.foo(123, .44d, 0X333l, 'a', "bc")
+		int i = 0; int j = 0;
+		f"""$i%dxyz"""
+	}
+}

744142898e6d9cce5c185ac22981b42bca34bd15
jonathan poelen <jonathan.poelen@gmail.com>
2021-07-04 16:56:52 +0000

    move coroutine keywords in controlflow and replace some keyword rule with WordDetect

--- autotests/input/highlight.cpp
+++ autotests/input/highlight.cpp
@@ -152,6 +152,14 @@ uint64_t namespaces()
     other::vector<T>;
 }
 
+task<> tcp_echo_server() {
+  char data[1024];
+  for (;;) {
+    size_t n = co_await socket.async_read_some(buffer(data));
+    co_await async_write(socket, buffer(data, n));
+  }
+}
+
 #if 1
     double foo(const A);
 #else // else

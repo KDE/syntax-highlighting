KDE/syntax-highlighting

commit 14c8f9765843217b31c969670ebcf61b6bad446b

Python: add bytes literals
- b'' (B-String) and rb''/br'' (Raw B-String)
- remove ru string prefix (does not exist)
- \ at the end of the string like String Char
- fix \" and \' with """...""" and '''...''' (error introduced by the previous commit)

autotests/folding/test.py.fold
@@ -45,6 +45,18 @@ def test_f_literals<beginfold id='2'>(</beginfold id='2'><endfold id='2'>)</endf
     f'x{bar<beginfold id='5'>[</beginfold id='5'>"baz"<endfold id='5'>]</endfold id='5'>:.2}y\n'
 
 a = "Escapes in String \U12345678 \xAB \""
+</indentfold>a = <beginfold id='1'>u'''</beginfold id='1'>\'''
+<indentfold><endfold id='1'>'''</endfold id='1'>
+a = <beginfold id='1'>u'''</beginfold id='1'>\'<endfold id='1'>'''</endfold id='1'>
+a = b'\u1234a\xffé\12k\n\g\
+'
+a = rb'\u1234aé\n\g\
+'
+a = <beginfold id='3'>b"""</beginfold id='3'><endfold id='3'>"""</endfold id='3'>
+a = <beginfold id='3'>b"""</beginfold id='3'>\"<endfold id='3'>"""</endfold id='3'>
+a = 'a' \
+    u'\x12'
+
 "Escapes in Doc/Comment String \u1234 \xAb \"\\"
 <beginfold id='1'>'''</beginfold id='1'> Doc/Comment String \N{123} \'<endfold id='1'>'''</endfold id='1'>
 <beginfold id='3'>"""</beginfold id='3'> Doc/Comment String \17 \x1f \n \" \\<endfold id='3'>"""</endfold id='3'>

autotests/html/test.py.dark.html
@@ -35,7 +35,7 @@ c <span style="color:#3f8058;">=</span> <span style="color:#f67400;">0x420_F52</
         <span style="color:#7f8c8d;">print</span>(<span style="color:#f44f4f;">&quot;&quot;&quot;multi</span>
 <span style="color:#f44f4f;">            line</span>
 <span style="color:#f44f4f;">            string&quot;&quot;&quot;</span>)
-        <span style="color:#7f8c8d;">print</span>(<span style="color:#f44f4f;">&quot;single \</span>
+        <span style="color:#7f8c8d;">print</span>(<span style="color:#f44f4f;">&quot;single </span><span style="color:#3daee9;">\</span>
 <span style="color:#f44f4f;">                continued line &quot;</span>)
         <span style="color:#7f8c8d;">print</span>(<span style="color:#f44f4f;">'single line'</span>)
     <span style="color:#fdbc4b;font-weight:bold;">return</span> <span style="color:#7f8c8d;">float</span>(<span style="color:#7f8c8d;">len</span>(a2))
@@ -51,6 +51,18 @@ c <span style="color:#3f8058;">=</span> <span style="color:#f67400;">0x420_F52</
     <span style="color:#da4453;">f'x</span><span style="color:#3daee9;">{</span>bar[<span style="color:#f44f4f;">&quot;baz&quot;</span>]<span style="color:#3daee9;">:.2}</span><span style="color:#da4453;">y</span><span style="color:#3daee9;">\n</span><span style="color:#da4453;">'</span>
 
 a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">&quot;Escapes in String </span><span style="color:#3daee9;">\U12345678</span><span style="color:#f44f4f;"> </span><span style="color:#3daee9;">\xAB</span><span style="color:#f44f4f;"> </span><span style="color:#3daee9;">\&quot;</span><span style="color:#f44f4f;">&quot;</span>
+a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">u'''</span><span style="color:#3daee9;">\'</span><span style="color:#f44f4f;">''</span>
+<span style="color:#f44f4f;">'''</span>
+a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">u'''</span><span style="color:#3daee9;">\'</span><span style="color:#f44f4f;">'''</span>
+a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">b'\u1234a</span><span style="color:#3daee9;">\xff</span><span style="color:#da4453;text-decoration:underline;">é</span><span style="color:#3daee9;">\12</span><span style="color:#f44f4f;">k</span><span style="color:#3daee9;">\n</span><span style="color:#f44f4f;">\g</span><span style="color:#3daee9;">\</span>
+<span style="color:#f44f4f;">'</span>
+a <span style="color:#3f8058;">=</span> <span style="color:#da4453;">rb'\u1234a</span><span style="color:#da4453;text-decoration:underline;">é</span><span style="color:#da4453;">\n\g\</span>
+<span style="color:#da4453;">'</span>
+a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">b&quot;&quot;&quot;&quot;&quot;&quot;</span>
+a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">b&quot;&quot;&quot;</span><span style="color:#3daee9;">\&quot;</span><span style="color:#f44f4f;">&quot;&quot;&quot;</span>
+a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">'a'</span> \
+    <span style="color:#f44f4f;">u'</span><span style="color:#3daee9;">\x12</span><span style="color:#f44f4f;">'</span>
+
 <span style="color:#7a7c7d;">&quot;Escapes in Doc/Comment String </span><span style="color:#3daee9;">\u1234</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\xAb</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\&quot;\\</span><span style="color:#7a7c7d;">&quot;</span>
 <span style="color:#7a7c7d;">''' Doc/Comment String </span><span style="color:#3daee9;">\N{123}</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\'</span><span style="color:#7a7c7d;">'''</span>
 <span style="color:#7a7c7d;">&quot;&quot;&quot; Doc/Comment String </span><span style="color:#3daee9;">\17</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\x1f</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\n</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\&quot;</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\\</span><span style="color:#7a7c7d;">&quot;&quot;&quot;</span>

autotests/html/test.py.html
@@ -35,7 +35,7 @@ c <span style="color:#ca60ca;">=</span> <span style="color:#b08000;">0x420_F52</
         <span style="color:#644a9b;font-weight:bold;">print</span>(<span style="color:#bf0303;">&quot;&quot;&quot;multi</span>
 <span style="color:#bf0303;">            line</span>
 <span style="color:#bf0303;">            string&quot;&quot;&quot;</span>)
-        <span style="color:#644a9b;font-weight:bold;">print</span>(<span style="color:#bf0303;">&quot;single \</span>
+        <span style="color:#644a9b;font-weight:bold;">print</span>(<span style="color:#bf0303;">&quot;single </span><span style="color:#924c9d;">\</span>
 <span style="color:#bf0303;">                continued line &quot;</span>)
         <span style="color:#644a9b;font-weight:bold;">print</span>(<span style="color:#bf0303;">'single line'</span>)
     <span style="font-weight:bold;">return</span> <span style="color:#644a9b;font-weight:bold;">float</span>(<span style="color:#644a9b;font-weight:bold;">len</span>(a2))
@@ -51,6 +51,18 @@ c <span style="color:#ca60ca;">=</span> <span style="color:#b08000;">0x420_F52</
     <span style="color:#ff5500;">f'x</span><span style="color:#3daee9;">{</span>bar[<span style="color:#bf0303;">&quot;baz&quot;</span>]<span style="color:#3daee9;">:.2}</span><span style="color:#ff5500;">y</span><span style="color:#924c9d;">\n</span><span style="color:#ff5500;">'</span>
 
 a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">&quot;Escapes in String </span><span style="color:#924c9d;">\U12345678</span><span style="color:#bf0303;"> </span><span style="color:#924c9d;">\xAB</span><span style="color:#bf0303;"> </span><span style="color:#924c9d;">\&quot;</span><span style="color:#bf0303;">&quot;</span>
+a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">u'''</span><span style="color:#924c9d;">\'</span><span style="color:#bf0303;">''</span>
+<span style="color:#bf0303;">'''</span>
+a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">u'''</span><span style="color:#924c9d;">\'</span><span style="color:#bf0303;">'''</span>
+a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">b'\u1234a</span><span style="color:#924c9d;">\xff</span><span style="color:#bf0303;text-decoration:underline;">é</span><span style="color:#924c9d;">\12</span><span style="color:#bf0303;">k</span><span style="color:#924c9d;">\n</span><span style="color:#bf0303;">\g</span><span style="color:#924c9d;">\</span>
+<span style="color:#bf0303;">'</span>
+a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">rb'\u1234a</span><span style="color:#bf0303;text-decoration:underline;">é</span><span style="color:#bf0303;">\n\g\</span>
+<span style="color:#bf0303;">'</span>
+a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">b&quot;&quot;&quot;&quot;&quot;&quot;</span>
+a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">b&quot;&quot;&quot;</span><span style="color:#924c9d;">\&quot;</span><span style="color:#bf0303;">&quot;&quot;&quot;</span>
+a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">'a'</span> \
+    <span style="color:#bf0303;">u'</span><span style="color:#924c9d;">\x12</span><span style="color:#bf0303;">'</span>
+
 <span style="color:#898887;">&quot;Escapes in Doc/Comment String </span><span style="color:#924c9d;">\u1234</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\xAb</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\&quot;\\</span><span style="color:#898887;">&quot;</span>
 <span style="color:#898887;">''' Doc/Comment String </span><span style="color:#924c9d;">\N{123}</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\'</span><span style="color:#898887;">'''</span>
 <span style="color:#898887;">&quot;&quot;&quot; Doc/Comment String </span><span style="color:#924c9d;">\17</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\x1f</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\n</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\&quot;</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\\</span><span style="color:#898887;">&quot;&quot;&quot;</span>

autotests/input/test.py
@@ -45,6 +45,18 @@ def test_f_literals():
     f'x{bar["baz"]:.2}y\n'
 
 a = "Escapes in String \U12345678 \xAB \""
+a = u'''\'''
+'''
+a = u'''\''''
+a = b'\u1234a\xffé\12k\n\g\
+'
+a = rb'\u1234aé\n\g\
+'
+a = b""""""
+a = b"""\""""
+a = 'a' \
+    u'\x12'
+
 "Escapes in Doc/Comment String \u1234 \xAb \"\\"
 ''' Doc/Comment String \N{123} \''''
 """ Doc/Comment String \17 \x1f \n \" \\"""

autotests/reference/test.py.ref
@@ -29,7 +29,7 @@
 <Normal Text>        </Normal Text><Builtin Function>print</Builtin Function><Normal Text>(</Normal Text><String>"""multi</String><br/>
 <String>            line</String><br/>
 <String>            string"""</String><Normal Text>)</Normal Text><br/>
-<Normal Text>        </Normal Text><Builtin Function>print</Builtin Function><Normal Text>(</Normal Text><String>"single \</String><br/>
+<Normal Text>        </Normal Text><Builtin Function>print</Builtin Function><Normal Text>(</Normal Text><String>"single </String><String Char>\</String Char><br/>
 <String>                continued line "</String><Normal Text>)</Normal Text><br/>
 <Normal Text>        </Normal Text><Builtin Function>print</Builtin Function><Normal Text>(</Normal Text><String>'single line'</String><Normal Text>)</Normal Text><br/>
 <Normal Text>    </Normal Text><Flow Control Keyword>return</Flow Control Keyword><Normal Text> </Normal Text><Builtin Function>float</Builtin Function><Normal Text>(</Normal Text><Builtin Function>len</Builtin Function><Normal Text>(a2))</Normal Text><br/>
@@ -45,6 +45,18 @@
 <Normal Text>    </Normal Text><F-String>f'x</F-String><String Substitution>{</String Substitution><Normal Text>bar[</Normal Text><String>"baz"</String><Normal Text>]</Normal Text><String Substitution>:.2}</String Substitution><F-String>y</F-String><String Char>\n</String Char><F-String>'</F-String><br/>
 <Normal Text></Normal Text><br/>
 <Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><String>"Escapes in String </String><String Char>\U12345678</String Char><String> </String><String Char>\xAB</String Char><String> </String><String Char>\"</String Char><String>"</String><br/>
+<Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><String>u'''</String><String Char>\'</String Char><String>''</String><br/>
+<String>'''</String><br/>
+<Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><String>u'''</String><String Char>\'</String Char><String>'''</String><br/>
+<Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><B-String>b'\u1234a</B-String><String Char>\xff</String Char><Error>é</Error><String Char>\12</String Char><B-String>k</B-String><String Char>\n</String Char><B-String>\g</B-String><String Char>\</String Char><br/>
+<B-String>'</B-String><br/>
+<Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><Raw B-String>rb'\u1234a</Raw B-String><Error>é</Error><Raw B-String>\n\g\</Raw B-String><br/>
+<Raw B-String>'</Raw B-String><br/>
+<Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><B-String>b""""""</B-String><br/>
+<Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><B-String>b"""</B-String><String Char>\"</String Char><B-String>"""</B-String><br/>
+<Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><String>'a'</String><Normal Text> \</Normal Text><br/>
+<Normal Text>    </Normal Text><String>u'</String><String Char>\x12</String Char><String>'</String><br/>
+<Normal Text></Normal Text><br/>
 <Comment>"Escapes in Doc/Comment String </Comment><String Char>\u1234</String Char><Comment> </Comment><String Char>\xAb</String Char><Comment> </Comment><String Char>\"\\</String Char><Comment>"</Comment><br/>
 <Comment>''' Doc/Comment String </Comment><String Char>\N{123}</String Char><Comment> </Comment><String Char>\'</String Char><Comment>'''</Comment><br/>
 <Comment>""" Doc/Comment String </Comment><String Char>\17</String Char><Comment> </Comment><String Char>\x1f</String Char><Comment> </Comment><String Char>\n</String Char><Comment> </Comment><String Char>\"</String Char><Comment> </Comment><String Char>\\</String Char><Comment>"""</Comment><br/>

commit e1c88423391c62448bea0009177228f90e20affa

Python: Fix r"\\" and derivatives
In such cases, the last \ should not be interpreted
as a line continuation escape.

This regression was introduced by 14c8f9765843217b31c969670ebcf61b6bad446b
and made more obvious by  90f5623611491ba665f1adeebff02916a73f37d6.

New regression tests are added for most variants, e.g. rb"""\\"""

BUG: 440412

autotests/folding/test.py.fold
@@ -61,6 +61,10 @@ a = 'a' \
     u'\x12'
 a = b"t\e's\t"
 a = rb"t\e's\t"
+a = r"\\" \
+rb'\\' \
+<beginfold id='3'>rf"""</beginfold id='3'>\\<endfold id='3'>"""</endfold id='3'> \
+<beginfold id='1'>ur'''</beginfold id='1'>\\<endfold id='1'>'''</endfold id='1'>
 
 "Escapes in Doc/Comment String \u1234 \xAb \"\\"
 <beginfold id='1'>'''</beginfold id='1'> Doc/Comment String \N{123} \'<endfold id='1'>'''</endfold id='1'>

autotests/html/test.py.dark.html
@@ -67,6 +67,10 @@ a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">'a'</span> 
     <span style="color:#f44f4f;">u'</span><span style="color:#3daee9;">\x12</span><span style="color:#f44f4f;">'</span>
 a <span style="color:#3f8058;">=</span> <span style="color:#f44f4f;">b&quot;t\e's</span><span style="color:#3daee9;">\t</span><span style="color:#f44f4f;">&quot;</span>
 a <span style="color:#3f8058;">=</span> <span style="color:#da4453;">rb&quot;t\e's\t&quot;</span>
+a <span style="color:#3f8058;">=</span> <span style="color:#da4453;">r&quot;\\&quot;</span> \
+<span style="color:#da4453;">rb'\\'</span> \
+<span style="color:#da4453;">rf&quot;&quot;&quot;\\&quot;&quot;&quot;</span> \
+<span style="color:#da4453;">ur'''\\'''</span>
 
 <span style="color:#7a7c7d;">&quot;Escapes in Doc/Comment String </span><span style="color:#3daee9;">\u1234</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\xAb</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\&quot;\\</span><span style="color:#7a7c7d;">&quot;</span>
 <span style="color:#7a7c7d;">''' Doc/Comment String </span><span style="color:#3daee9;">\N{123}</span><span style="color:#7a7c7d;"> </span><span style="color:#3daee9;">\'</span><span style="color:#7a7c7d;">'''</span>

autotests/html/test.py.html
@@ -67,6 +67,10 @@ a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">'a'</span> 
     <span style="color:#bf0303;">u'</span><span style="color:#924c9d;">\x12</span><span style="color:#bf0303;">'</span>
 a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">b&quot;t\e's</span><span style="color:#924c9d;">\t</span><span style="color:#bf0303;">&quot;</span>
 a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">rb&quot;t\e's\t&quot;</span>
+a <span style="color:#ca60ca;">=</span> <span style="color:#bf0303;">r&quot;\\&quot;</span> \
+<span style="color:#bf0303;">rb'\\'</span> \
+<span style="color:#bf0303;">rf&quot;&quot;&quot;\\&quot;&quot;&quot;</span> \
+<span style="color:#bf0303;">ur'''\\'''</span>
 
 <span style="color:#898887;">&quot;Escapes in Doc/Comment String </span><span style="color:#924c9d;">\u1234</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\xAb</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\&quot;\\</span><span style="color:#898887;">&quot;</span>
 <span style="color:#898887;">''' Doc/Comment String </span><span style="color:#924c9d;">\N{123}</span><span style="color:#898887;"> </span><span style="color:#924c9d;">\'</span><span style="color:#898887;">'''</span>

autotests/input/test.py
@@ -61,6 +61,10 @@ a = 'a' \
     u'\x12'
 a = b"t\e's\t"
 a = rb"t\e's\t"
+a = r"\\" \
+rb'\\' \
+rf"""\\""" \
+ur'''\\'''
 
 "Escapes in Doc/Comment String \u1234 \xAb \"\\"
 ''' Doc/Comment String \N{123} \''''

autotests/reference/test.py.ref
@@ -61,6 +61,10 @@
 <Normal Text>    </Normal Text><String>u'</String><String Char>\x12</String Char><String>'</String><br/>
 <Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><B-String>b"t\e's</B-String><String Char>\t</String Char><B-String>"</B-String><br/>
 <Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><Raw B-String>rb"t\e's\t"</Raw B-String><br/>
+<Normal Text>a </Normal Text><Operator>=</Operator><Normal Text> </Normal Text><Raw String>r"\\"</Raw String><Normal Text> \</Normal Text><br/>
+<Raw B-String>rb'\\'</Raw B-String><Normal Text> \</Normal Text><br/>
+<Raw F-String>rf"""\\"""</Raw F-String><Normal Text> \</Normal Text><br/>
+<Raw String>ur'''\\'''</Raw String><br/>
 <Normal Text></Normal Text><br/>
 <Comment>"Escapes in Doc/Comment String </Comment><String Char>\u1234</String Char><Comment> </Comment><String Char>\xAb</String Char><Comment> </Comment><String Char>\"\\</String Char><Comment>"</Comment><br/>
 <Comment>''' Doc/Comment String </Comment><String Char>\N{123}</String Char><Comment> </Comment><String Char>\'</String Char><Comment>'''</Comment><br/>

data/syntax/python.xml
@@ -52,7 +52,7 @@
 <!-- v2.07 add support for %prog and co, see bug 142832 -->
 <!-- v2.08 add missing overloaders, new Python 3 statements, builtins, and keywords -->
 <!-- v2.29 recognize escape sequenzes correctly -->
-<language name="Python" version="20" style="python" indenter="python" kateversion="5.0" section="Scripts" extensions="*.py;*.pyw;SConstruct;SConscript;*.FCMacro" mimetype="application/x-python;text/x-python;text/x-python3" casesensitive="1" author="Michael Bueker" license="">
+<language name="Python" version="21" style="python" indenter="python" kateversion="5.0" section="Scripts" extensions="*.py;*.pyw;SConstruct;SConscript;*.FCMacro" mimetype="application/x-python;text/x-python;text/x-python3" casesensitive="1" author="Michael Bueker" license="">
 	<highlighting>
 		<list name="import">
 			<item>import</item>
@@ -635,6 +635,7 @@
 				<DetectSpaces attribute="Raw String"/>
 				<DetectIdentifier attribute="Raw String"/>
 				<Detect2Chars attribute="Raw String" char="\" char1="'"/>
+				<Detect2Chars attribute="Raw String" char="\" char1="\"/>
 				<IncludeRules context="stringformat"/>
 				<StringDetect attribute="Raw String" String="'''" context="#pop!#CheckForString" endRegion="Triple A-region"/>
 			</context>
@@ -652,6 +653,7 @@
 				<DetectSpaces attribute="Raw F-String"/>
 				<DetectIdentifier attribute="Raw F-String"/>
 				<Detect2Chars attribute="Raw F-String" char="\" char1="'"/>
+				<Detect2Chars attribute="Raw F-String" char="\" char1="\"/>
 				<IncludeRules context="stringinterpolation"/>
 				<StringDetect attribute="Raw F-String" String="'''" context="#pop!#CheckForString" endRegion="Triple A-region"/>
 			</context>
@@ -684,6 +686,7 @@
 				<DetectSpaces attribute="Raw String"/>
 				<DetectIdentifier attribute="Raw String"/>
 				<Detect2Chars attribute="Raw String" char="\" char1="&quot;"/>
+				<Detect2Chars attribute="Raw String" char="\" char1="\"/>
 				<IncludeRules context="stringformat"/>
 				<StringDetect attribute="Raw String" String="&quot;&quot;&quot;" context="#pop!#CheckForString" endRegion="Triple Q-region"/>
 			</context>
@@ -701,6 +704,7 @@
 				<DetectSpaces attribute="Raw F-String"/>
 				<DetectIdentifier attribute="Raw F-String"/>
 				<Detect2Chars attribute="Raw F-String" char="\" char1="&quot;"/>
+				<Detect2Chars attribute="Raw F-String" char="\" char1="\"/>
 				<IncludeRules context="stringinterpolation"/>
 				<StringDetect attribute="Raw F-String" String="&quot;&quot;&quot;" context="#pop!#CheckForString" endRegion="Triple Q-region"/>
 			</context>
@@ -734,6 +738,7 @@
 				<DetectSpaces attribute="Raw String"/>
 				<DetectIdentifier attribute="Raw String"/>
 				<Detect2Chars attribute="Raw String" char="\" char1="'"/>
+				<Detect2Chars attribute="Raw String" char="\" char1="\"/>
 				<IncludeRules context="stringformat"/>
 				<DetectChar attribute="Raw String" char="'" context="#pop!#CheckForString"/>
 				<LineContinue attribute="Raw String" context="#stay"/>
@@ -752,6 +757,7 @@
 				<DetectSpaces attribute="Raw F-String"/>
 				<DetectIdentifier attribute="Raw F-String"/>
 				<Detect2Chars attribute="Raw F-String" char="\" char1="'"/>
+				<Detect2Chars attribute="Raw F-String" char="\" char1="\"/>
 				<IncludeRules context="stringinterpolation"/>
 				<DetectChar attribute="Raw F-String" char="'" context="#pop!#CheckForString"/>
 				<LineContinue attribute="Raw F-String" context="#stay"/>
@@ -786,6 +792,7 @@
 				<DetectSpaces attribute="Raw String"/>
 				<DetectIdentifier attribute="Raw String"/>
 				<Detect2Chars attribute="Raw String" char="\" char1="&quot;"/>
+				<Detect2Chars attribute="Raw String" char="\" char1="\"/>
 				<IncludeRules context="stringformat"/>
 				<DetectChar attribute="Raw String" char="&quot;" context="#pop!#CheckForString"/>
 				<LineContinue attribute="Raw String" context="#stay"/>
@@ -804,6 +811,7 @@
 				<DetectSpaces attribute="Raw F-String"/>
 				<DetectIdentifier attribute="Raw F-String"/>
 				<Detect2Chars attribute="Raw F-String" char="\" char1="&quot;"/>
+				<Detect2Chars attribute="Raw F-String" char="\" char1="\"/>
 				<IncludeRules context="stringinterpolation"/>
 				<DetectChar attribute="Raw F-String" char="&quot;" context="#pop!#CheckForString"/>
 				<LineContinue attribute="Raw F-String" context="#stay"/>
@@ -820,6 +828,7 @@
 			<context name="Raw Q-B-String" attribute="Raw B-String" lineEndContext="#pop!UnfinishedStringError">
 				<RegExpr attribute="Raw B-String" String="([\x20\x21\x23-\x5B\x5D-\x7E]++|\\.)++"/>
 				<DetectChar attribute="Raw B-String" char="&quot;" context="#pop!#CheckForString"/>
+				<Detect2Chars attribute="Raw B-String" char="\" char1="\"/>
 				<LineContinue attribute="Raw B-String" context="#stay"/>
 				<RegExpr attribute="Error" String="."/>
 			</context>
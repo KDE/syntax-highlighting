8702403c89b7f621373fd28b2a3b7735629b9764
Waqar Ahmed <waqar.17a@gmail.com>
2023-01-17 12:46:20 +0500

    Use DetectChar and Detect2Chars instead of Regexp

--- autotests/input/highlight.v
+++ autotests/input/highlight.v
@@ -4,6 +4,12 @@
  */
 fn main() {
 	mut s := ''
+	mut str := 'asd\n'
+	mut str := 'asd \' asd'
+	mut str := "asd \" asd"
+	mut ch := `a`
+	mut n := 1.2
+	mut d := 12
 	for n in 1 .. 101 {
 		if n % 3 == 0 {
 			s += 'Fizz'

f3e3681ce766d44c162a3468ad2984feeb4d3d7b
Jonathan Poelen <jonathan.poelen@gmail.com>
2024-07-31 00:15:41 +0200

    Python: raw-string with lowercase r are highlighted as regex
    This applies to rb and rf variants, but not to the versions with r, b or f in uppercase.

--- autotests/input/test.py
+++ autotests/input/test.py
@@ -55,18 +55,397 @@ a = u'''\''''
 a = b'\u1234a\xffé\12k\n\g\
 s\
 \"\ '
-a = rb'\u1234aé\n\g\
+a = Rb'\u1234aé\n\g\
 '
 a = b""""""
 a = b"""\""""
 a = 'a' \
     u'\x12'
 a = b"t\e's\t"
-a = rb"t\e's\t"
-a = r"\\" \
-rb'\\' \
-rf"""\\""" \
-ur'''\\'''
+a = Rb"t\e's\t"
+a = R"\\" \
+Rb'\\' \
+Rf"""\\""" \
+uR'''\\'''
+
+# regex (re and regex package)
+#{
+# r'...'
+patt = r'a+é??a?*a{x,}a{,}a{1}a{1,}a{,2}a{1,2}\g<12>\L<name>\g<name>\w.\'\b\\a#$'
+             # ~~ invalid, but ok
+patt = r'\p{H}\x12\u1234\1\023\0234\123\1234\12'
+patt = r'(a)(?:a)(?=a)(?R)(?r)(?a-m:a)(?oups)(?P<name>a)(?|a|b)(?#comment\)\'\")(?>=a)\
+    (?(1)a|b))a(?(?!a)a|b)'
+      # oups ~
+patt = r'[^xy-z]|[]xy[:alpha:]z]|[[\wa(){}\b\]\[\..]|[[a-z]--[c]]'
+                                          # regex V1 ~~~~~~~~~~~~
+patt = r'(((a)))(?:(?!(?<a>a("\'\"))))'
+patt = r'(?:(?!(?<a>a(\
+.' # break
+
+# r"..."
+patt = r"a+é??a?*a{x,}a{,}a{1}a{1,}a{,2}a{1,2}\g<12>\L<name>\g<name>\w.\"\b\\a#$"
+             # ~~ invalid, but ok
+patt = r"\p{H}\x12\u1234\1\023\0234\123\1234\12"
+patt = r"(a)(?:a)(?=a)(?R)(?r)(?a-m:a)(?oups)(?P<name>a)(?|a|b)(?#comment\)\'\")\
+    (?>=a)(?(1)a|b))a(?(?!a)a|b)"
+            # oups ~
+patt = r"[^xy-z]|[]xy[:alpha:]z]|[[\wa(){}\b\]\[\..]|[[a-z]--[c]]"
+                                          # regex V1 ~~~~~~~~~~~~
+patt = r"(((a)))(?:(?!(?<a>a(\'\"'))))"
+patt = r"(?:(?!(?<a>a(\
+." # break
+
+# r'''...'''
+patt = r'''
+    a+é??a?*
+        # ~~ invalid, but ok
+    a{x,}a{{,}}a{1}a{1,}a{,2}a{1,2}  # bla bla
+    \g<12>\L<name>\g<name>
+    \w.''\'\b\\a\#\p{H}\x12\u1234\1\023\0234\123\1234\12$
+'''
+patt = r'''
+    (a)(?:a)
+    (?=a)(?R)(?r)(?a-m:a)(?ou\
+        ps)(?P<name>a)(?|a|b)
+    a(?#comment\)\'\"''"")(?>=a)
+    (?#bla
+     bla)abc
+    (?(1)a|b))a(?(?!a)a|b)
+      # oups ~
+'''
+patt = r'''
+    [^xy-\
+        z]
+    []xy[:alpha:]
+    z]
+    [[\wa(){}\b\]\[\..]
+    [[a-z]--[c]] # regex V1
+'''
+patt = r'''(
+    (
+        (
+            a # bla
+        )
+    )
+)(?:
+    (?!
+        (?<a>a(
+            '" # bla
+            abc\
+                z
+        ))
+    )
+)'''
+patt = r'''
+    (?:
+         (?!
+            (?<a>a(
+.''' # break
+
+# r"""..."""
+patt = r"""
+    a+é??a?*
+        # ~~ invalid, but ok
+    a{x,}a{{,}}a{1}a{1,}a{,2}a{1,2}  # bla bla
+    \g<12>\L<name>\g<name>
+    \w.""\"\b\\a\#\p{H}\x12\u1234\1\023\0234\123\1234\12$
+"""
+patt = r"""
+    (a)(?:a)
+    (?=a)(?R)(?r)(?a-m:a)(?ou\
+        ps)(?P<name>a)(?|a|b)
+    a(?#comment\)\'\")(?>=a)
+    (?#bla
+     bla)abc
+    (?(1)a|b))a(?(?!a)a|b)
+      # oups ~
+"""
+patt = r"""
+    [^xy-\
+        z]
+    []xy[:alpha:]
+    z]
+    [[\wa(){}\b\]\[\..] # bla bla
+    [[a-z]--[c]] # regex V1
+"""
+patt = r"""(
+    (
+        (
+            a # bla
+        )
+    )
+)(?:
+    (?!
+        (?<a>a(
+            '" # bla
+            abc\
+                z
+        ))
+    )
+)"""
+patt = r"""
+    (?:
+         (?!
+            (?<a>a(
+.""" # break
+
+# rb'...'
+patt = rb'a+é??a?*a{x,}a{,}a{1}a{1,}a{,2}a{1,2}\g<12>\L<name>\g<name>\w.\'\b\\a#$'
+     # oups ~   ~~ invalid, but ok
+patt = rb'\p{H}\x12\u1234\1\023\0234\123\1234\12'
+        # ~~ oups  ~~
+patt = br'(a)(?:a)(?=a)(?R)(?r)(?a-m:a)(?oups)(?P<name>a)(?|a|b)(?#comment\)\'\")\
+    (?>=a)(?(1)a|b))a(?(?!a)a|b)'
+            # oups ~
+patt = rb'[^xy-z]|[]xy[:alpha:]z]|[[\wa(){}\b\]\[\..]|[[a-z]--[c]]'
+                                           # regex V1 ~~~~~~~~~~~~
+patt = br'(((a)))(?:(?!(?<a>a("\'\"))))'
+patt = rb'(?:(?!(?<a>a(\
+.' # break
+
+# rb"..."
+patt = rb"a+é??a?*a{x,}a{,}a{1}a{1,}a{,2}a{1,2}\g<12>\L<name>\g<name>\w.\"\b\\a#$"
+     # oups ~   ~~ invalid, but ok
+patt = rb"\p{H}\x12\u1234\1\023\0234\123\1234\12"
+        # ~~ oups  ~~
+patt = br"(a)(?:a)(?=a)(?R)(?r)(?a-m:a)(?oups)(?P<name>a)(?|a|b)(?#comment\)\'\")\
+    (?>=a)(?(1)a|b))a(?(?!a)a|b)"
+            # oups ~
+patt = rb"[^xy-z]|[]xy[:alpha:]z]|[[\wa(){}\b\]\[\..]|[[a-z]--[c]]"
+                                           # regex V1 ~~~~~~~~~~~~
+patt = br"(((a)))(?:(?!(?<a>a(\'\"'))))"
+patt = rb"(?:(?!(?<a>a(\
+." # break
+
+
+# rb'''...'''
+patt = rb'''
+    a+é??a?*
+        # ~~ invalid, but ok
+    a{x,}a{,}a{1}a{1,}a{,2}a{1,2}  # bla bla
+    \g<12>\L<name>\g<name>
+    \w.''\'\b\\a\#\p{H}\x12\u1234\1\023\0234\123\1234\12$
+'''
+patt = br'''
+    (a)(?:a)
+    (?=a)(?R)(?r)(?a-m:a)(?ou\
+        ps)(?P<name>a)(?|a|b)
+    a(?#comment\)\'\"''"")(?>=a)
+    (?#bla
+     bla)abc
+    (?(1)a|b))a(?(?!a)a|b)
+      # oups ~
+'''
+patt = rb'''
+    [^xy-\
+        z]
+    []xy[:alpha:]
+    z]
+    [[\wa(){}\b\]\[\..]
+    [[a-z]--[c]] # regex V1
+'''
+patt = br'''(
+    (
+        (
+            a # bla
+        )
+    )
+)(?:
+    (?!
+        (?<a>a(
+            '" # bla
+            abc\
+                z
+        ))
+    )
+)'''
+patt = rb'''
+    (?:
+         (?!
+            (?<a>a(
+.''' # break
+
+# rb"""..."""
+patt = rb"""
+    a+é??a?*
+        # ~~ invalid, but ok
+    a{x,}a{,}a{1}a{1,}a{,2}a{1,2}  # bla bla
+    \g<12>\L<name>\g<name>
+    \w.""\"\b\\a\#\p{H}\x12\u1234\1\023\0234\123\1234\12$
+"""
+patt = br"""
+    (a)(?:a)
+    (?=a)(?R)(?r)(?a-m:a)(?ou\
+        ps)(?P<name>a)(?|a|b)
+    a(?#comment\)\'\")(?>=a)
+    (?#bla
+     bla)abc
+    (?(1)a|b))a(?(?!a)a|b)
+      # oups ~
+"""
+patt = rb"""
+    [^xy-\
+        z]
+    []xy[:alpha:]
+    z]
+    [[\wa(){}\b\]\[\..] # bla bla
+    [[a-z]--[c]] # regex V1
+"""
+patt = br"""(
+    (
+        (
+            a # bla
+        )
+    )
+)(?:
+    (?!
+        (?<a>a(
+            '" # bla
+            abc\
+                z
+        ))
+    )
+)"""
+patt = rb"""
+    (?:
+         (?!
+            (?<a>a(
+.""" # break
+
+# rf'...'
+patt = rf'a+é??a?*\L<name>\L<{name}>\g<name>\g<{name}>\g<12>\w.\'\b\\a#'
+              # ~~ invalid, but ok
+patt = rf'\p{{H}}\p{name}\p{{{name}}}\x12\u1234\1\023\0234\123\1234\12$'
+patt = rf'a{n}a{{x,}}a{{,}}a{{1}}a{{1,}}a{{,2}}a{{1,2}}a{{{n}}}a{{{n},}}a{{,{n}}}a{{{n},{n}}}'
+patt = fr'(a)(?:a)(?=a)(?R)(?r)(?a-m:a)(?oups)(?P<name>a)(?|a|b)(?#comment\)\'\")\
+    (?>=a)(?(1)a|b))a(?(?!a)a|b)'
+            # oups ~
+patt = fr'({x})(?={x})(?{x})(?{x}:a)(?oups)(?P<{x}>a)(?({x})a|b))a(?(?!{x}){y}|{y})'
+                                                         # oups ~
+patt = rf'[^x${x}y-z]|[]xy[:alpha:]z]|[[\wa}(){{}}\b\]\[\..]|[[a-z]--[c]]'
+                                                  # regex V1 ~~~~~~~~~~~~
+patt = fr'(((a)))(?:(?!(?<a>${x}a("\'\"))))'
+patt = rf'(?:(?!(?<a>a(\
+.${x}' # break
+
+# rf"..."
+patt = rf"a+é??a?*\L<name>\L<{name}>\g<name>\g<{name}>\g<12>\w.\'\b\\a#"
+              # ~~ invalid, but ok
+patt = rf"\p{{H}}\p{name}\p{{{name}}}\x12\u1234\1\023\0234\123\1234\12$"
+patt = rf"a{n}a{{x,}}a{{,}}a{{1}}a{{1,}}a{{,2}}a{{1,2}}a{{{n}}}a{{{n},}}a{{,{n}}}a{{{n},{n}}}"
+patt = fr"(a)(?:a)(?=a)(?R)(?r)(?a-m:a)(?oups)(?P<name>a)(?|a|b)(?#comment\)\'\")\
+    (?>=a)(?(1)a|b))a(?(?!a)a|b)"
+            # oups ~
+patt = fr"({x})(?={x})(?{x})(?{x}:a)(?oups)(?P<{x}>a)(?({x})a|b))a(?(?!{x}){y}|{y})"
+                                                         # oups ~
+patt = rf"[^x${x}y-z]|[]xy[:alpha:]z]|[[\wa}(){{}}\b\]\[\..]|[[a-z]--[c]]"
+                                                  # regex V1 ~~~~~~~~~~~~
+patt = fr"(((a)))(?:(?!(?<a>${x}a('\"'))))"
+patt = rf"(?:(?!(?<a>a(\
+.${x}" # break
+
+
+# rf'''...'''
+patt = rf'''
+    a+é??a?*
+        # ~~ invalid, but ok
+    a{n}a{{x,}}a{{,}}a{{1}}a{{1,}}a{{,2}}a{{1,2}}  # bla bla
+    a{{{n}}}a{{{n},}}a{{,{n}}}a{{{n},{n}}}
+    \L<name>\L<{name}>\g<name>\g<{name}>\g<12>
+    \w.''\'\b\\a\#\p{{H}}\p{name}\p{{{name}}}\x12\u1234\1\023\0234\123\1234\12$
+'''
+patt = fr'''
+    (a)(?:a)
+    (?=a)(?R)(?r)(?a-m:a)(?ou\
+        ps)(?P<name>a)(?|a|b)
+    a(?#comment\)\'\"''"")(?>=a)
+    (?#bla {} {a} {{a}} }
+     bla)abc
+    (?(1)a|b))a(?(?!a)a|b)
+      # oups ~
+    ({x})(?={x})(?{x})(?{x}:a)(?oups)(?P<{x}>a)(?({x})a|b))a(?(?!{x}){y}|{y})
+                                                   # oups ~
+'''
+patt = rf'''
+    [^x${x}y-\
+        z]
+    []xy[:alpha:]
+    z]
+    [[\wa}(){{}}\b\]\[\..]
+    [[a-z]--[c]] # regex V1
+'''
+patt = fr'''(
+    (
+        (
+            a # bla
+        )
+    )
+)(?:
+    (?!
+        (?<a>a(
+            '" # bla
+            abc\
+                z
+        ))
+    )
+)'''
+patt = rf'''
+    (?:
+         (?!
+            (?<a>${x}a(
+.''' # break
+
+# rf"""..."""
+patt = rf"""
+    a+é??a?*
+        # ~~ invalid, but ok
+    a{n}a{{x,}}a{{,}}a{{1}}a{{1,}}a{{,2}}a{{1,2}}  # bla bla
+    a{{{n}}}a{{{n},}}a{{,{n}}}a{{{n},{n}}}
+    \L<name>\L<{name}>\g<name>\g<{name}>\g<12>
+    \w.""\"\b\\a\#\p{{H}}\p{name}\p{{{name}}}\x12\u1234\1\023\0234\123\1234\12$
+"""
+patt = fr"""
+    (a)(?:a)
+    (?=a)(?R)(?r)(?a-m:a)(?ou\
+        ps)(?P<name>a)(?|a|b)
+    a(?#comment\)\'\")(?>=a)
+    (?#bla {} {a} {{a}} }
+     bla)abc
+    (?(1)a|b))a(?(?!a)a|b)
+      # oups ~
+    ({x})(?={x})(?{x})(?{x}:a)(?oups)(?P<{x}>a)(?({x})a|b))a(?(?!{x}){y}|{y})
+                                                   # oups ~
+"""
+patt = rf"""
+    [^x${x}y-\
+        z]
+    []xy[:alpha:]
+    z]
+    [[\wa}(){{}}\b\]\[\..] # bla bla
+    [[a-z]--[c]] # regex V1
+"""
+patt = fr"""(
+    (
+        (
+            a # bla
+        )
+    )
+)(?:
+    (?!
+        (?<a>a(
+            '" # bla
+            abc\
+                z
+        ))
+    )
+)"""
+patt = rf"""
+    (?:
+         (?!
+            (?<a>${x}a(
+.""" # break
+#}
 
 "Escapes in Doc/Comment String \u1234 \xAb \"\\"
 ''' Doc/Comment String \N{123} \''''
@@ -140,7 +519,7 @@ print('dsdsa' \
 "
 a = 'otherwise the next line will be
 stuck on the Error attribute'
-br"Strings delimited by single quotes (' or ")
+BR"Strings delimited by single quotes (' or ")
 can NOT be defined across multiple lines
 unless escaped with \
 "
